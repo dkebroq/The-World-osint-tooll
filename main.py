f862984f1c5ca2b3bae54e0c6e6bf079494613b4
dkebroq <diallegenda@gmail.com>
2025-04-28 18:20:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import phonenumbers
+from phonenumbers import carrier, geocoder
 
+def analyze_phone(phone):
+    num = phonenumbers.parse(phone)
+    print(f"Страна: {geocoder.description_for_number(num, 'ru')}")
+    print(f"Оператор: {carrier.name_for_number(num, 'ru')}")
+
+if __name__ == "__main__":
+    analyze_phone("+79123456789")
